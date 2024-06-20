52160f4837aa22329f64da059cacfcbf089ad58f
John McEwan <bonzojo@proton.me>
2024-06-20 05:58:32 +0100

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import bbLogo from './assets/BBLogo2018.png'
 import './App.css'
 
 function App() {
@@ -9,25 +8,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="" target="_blank">
+          <img src={bbLogo} className="logo" alt="Blood Bowl logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Grungle Cup - A Blood Bowl Tournament</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
     </>
   )
 }
